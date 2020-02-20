a4e254f1e5a5630351a644accf7f0693370a3596
Alex Lee <alyc100@hotmail.com>
2020-02-20 17:08:29 +0000

    remove apps

--- buhlerapp/data/channels.js
+++
@@ -1,8 +0,0 @@
-window.channel = {
-    "assisted": ["Destino CRM", "CEP"],
-    "social": ["Twitter Ad", "YouTube Organic", "Facebook Ad", "YouTube Ad", "Facebook", "Twitter"],
-    "outbound": ["Google Org", "Google SEA", "Email"],
-    "web": ["Destino Web"],
-    "physical": [],
-    "mobile": []
-};
--- marstonsdashboard/data/channels.js
+++
@@ -1,8 +0,0 @@
-window.channel = {
-    "assisted": ["Demo CRM", "CEP"],
-    "social": ["Twitter Ad", "YouTube Organic", "Facebook Ad", "YouTube Ad", "Facebook", "Twitter"],
-    "outbound": ["Google Org", "Google SEA", "Email"],
-    "web": ["Marstons Inn", "Guestline", "Boundary Stone Pub"],
-    "physical": ["Marstons Pub"], 
-    "mobile": []
-};
