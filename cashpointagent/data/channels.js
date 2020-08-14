96295664e59f13dbc076632f02524e6f2113cd19
Alex Lee <alyc100@hotmail.com>
2020-08-14 10:49:06 +0100

    cashpointagent

---
+++ cashpointagent/data/channels.js
@@ -0,0 +1,8 @@
+window.channel = {
+    "assisted": ["Destino CRM", "CEP"],
+    "social": ["Twitter Ad", "YouTube Organic", "Facebook Ad", "YouTube Ad", "Facebook", "Twitter"],
+    "outbound": ["Google Org", "Google SEA", "Email"],
+    "web": ["Destino Web"],
+    "physical": ["Destino Travelstore"], 
+    "mobile": []
+};
