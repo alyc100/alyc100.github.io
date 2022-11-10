d3904589eaf9eb9e726d54523a35e14a7762538c
alyc100 <alyc100@hotmail.com>
2022-11-10 08:46:35 +0000

    delete websites

--- w/boots_deliveroo_files/config.js
+++
@@ -1,6 +0,0 @@
-
-window[window["TiktokAnalyticsObject"]].instance("C8CBIFSN9V2G1TDUK3HG").setAdvancedMatchingAvailableProperties({"email":true,"phone_number":true,"auto_email":true,"auto_phone_number":true});
-window[window["TiktokAnalyticsObject"]].instance("C8CBIFSN9V2G1TDUK3HG").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("C8CBIFSN9V2G1TDUK3HG").setPixelInfo({status: 0, name: "Boots Pixel", advertiserID: "6956120149648752641", setupMode: 1, partner: "", is_onsite: false });
-window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("boots.com");
-window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig(null);
-window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie && window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie(true);
